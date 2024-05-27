0e56e06e220de57c4595bf913260ada203f9eda9
Colin Leary <colinleary123@gmail.com>
2024-05-27 10:07:01 -0400

    reset

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,2 +1,25 @@
 package api
 
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+type APIserver struct {
+	addr string 
+	db *sql.DB
+}
+
+func NewAPIsever(addr string, db *sql.DB) *APIserver{
+	return &APIserver{
+		addr: addr,
+		db: db,
+	}
+}
+
+func (s *APIserver) start() error {
+	router := mux.NewRouter()
+
+	return http.ListenAndServe(s.addr, router)
+}
